784708c1a0899c7b44ceb85ab9218020b15a1036
Aaliyah Iysha El Ghilani Fryatt <123201315+iysha22@users.noreply.github.com>
2024-06-14 20:22:03 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <h1>Start Bootstrap</h1>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+        <section className="hero">
+          <h2>A Warm Welcome!</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
+          <button>Call to action!</button>
+        </section>
+        <section className="cards">
+          <div className="card">
+            <img src="https://via.placeholder.com/500x325" alt="Placeholder" />
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+          <div className="card">
+            <img src="https://via.placeholder.com/500x325" alt="Placeholder" />
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+          <div className="card">
+            <img src="https://via.placeholder.com/500x325" alt="Placeholder" />
+            <h3>Card title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </div>
+        </section>
+      </header>
+    </div>
+  );
 };
 
 export default Home;
